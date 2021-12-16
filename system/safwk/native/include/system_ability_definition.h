d5967594e538673214ab3aa67dadb77ebce6fd42
Gymee <yumeijie@huawei.com>
2021-12-16 14:20:42 +0800

    add device profile SA ID
    Signed-off-by: Gymee <yumeijie@huawei.com>
    Change-Id: I5439ca45004eabb2517e3acb4b1ace135e82fee7

--- system/safwk/native/include/system_ability_definition.h
+++ system/safwk/native/include/system_ability_definition.h
@@ -201,6 +201,7 @@ enum {
     DEVICE_STORAGE_MANAGER_SERVICE_ID                = 5000,
     STORAGE_SERVICE_ID                               = 5001,
     DEVICE_SERVICE_MANAGER_SA_ID                     = 5100,
+    DISTRIBUTED_DEVICE_PROFILE_SA_ID                 = 6001,
     LAST_SYS_ABILITY_ID                              = 0x00ffffff,  // 16777215
 };
 
@@ -244,6 +245,7 @@ static const std::map<int, std::string> saNameMap_ = {
     { DISTRIBUTED_HARDWARE_DEVICEMANAGER_SA_ID, "DeviceManagerService" },
     { USB_SYSTEM_ABILITY_ID, "UsbService" },
     { DEVICE_SERVICE_MANAGER_SA_ID, "HdfDeviceServiceManager" },
+    { DISTRIBUTED_DEVICE_PROFILE_SA_ID, "DistributedDeviceProfile" },
 };
 } // namespace OHOS
 
